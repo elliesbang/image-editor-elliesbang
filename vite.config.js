c6b9c1eaae402658a771ac0c2a02c37dbeb3f3eb
elliesbang <shandbr1996@gmail.com>
2025-11-01 16:20:43 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+// ✅ Cloudflare Pages용 상대 경로 설정
 export default defineConfig({
   plugins: [react()],
+  base: "./", // ⚠️ 이 한 줄이 핵심!
 });
