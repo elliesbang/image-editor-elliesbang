706d9ce8968263f4b42facfd1cf879cf69d1e266
elliesbang <shandbr1996@gmail.com>
2025-11-03 19:35:59 +0900

    Create index.js

---
+++ src/components/AdditionalTool/index.js
@@ -0,0 +1,4 @@
+export { default as ResizeTool } from "./ResizeTool.jsx";
+export { default as SvgConvertTool } from "./SvgConvertTool.jsx";
+export { default as GifConvertTool } from "./GifConvertTool.jsx";
+export { default as KeywordAnalyzeTool } from "./KeywordAnalyzeTool.jsx";
