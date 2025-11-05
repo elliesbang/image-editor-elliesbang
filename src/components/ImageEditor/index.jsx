7e4e02ff118ff4d72d014222b3cc77cb91f2285c
elliesbang <shandbr1996@gmail.com>
2025-11-05 19:28:02 +0900

    Update index.jsx

--- src/components/ImageEditor/index.jsx
+++ src/components/ImageEditor/index.jsx
@@ -4,16 +4,38 @@ import RemoveBgCropButton from "./RemoveBgCropButton";
 import CropButton from "./CropButton";
 import DenoiseButton from "./DenoiseButton";
 
-export default function ImageEditor({ selectedImage }) {
-  const hasImage = !!selectedImage;
+export default function ImageEditor({ selectedImage, selectedImages = [] }) {
+  // 단일 이미지 또는 여러 이미지 중 하나라도 있으면 true
+  const hasImage = !!selectedImage || selectedImages.length > 0;
+
+  // 배열 전달 보장 (selectedImages가 비었으면 단일 이미지라도 포함)
+  const activeImages =
+    selectedImages.length > 0
+      ? selectedImages
+      : selectedImage
+      ? [selectedImage]
+      : [];
 
   return (
     <div className="editor-section">
       <div className="button-grid">
-        <RemoveBgButton selectedImage={selectedImage} disabled={!hasImage} />
-        <RemoveBgCropButton selectedImage={selectedImage} disabled={!hasImage} />
-        <CropButton selectedImage={selectedImage} disabled={!hasImage} />
-        <DenoiseButton selectedImage={selectedImage} disabled={!hasImage} />
+        {/* ✅ 수정된 부분들: 배열 전달 */}
+        <RemoveBgButton
+          selectedImages={activeImages}
+          disabled={!hasImage}
+        />
+        <RemoveBgCropButton
+          selectedImages={activeImages}
+          disabled={!hasImage}
+        />
+        <CropButton
+          selectedImages={activeImages}
+          disabled={!hasImage}
+        />
+        <DenoiseButton
+          selectedImages={activeImages}
+          disabled={!hasImage}
+        />
       </div>
 
       {!hasImage && (
